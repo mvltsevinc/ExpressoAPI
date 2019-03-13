e1146753f5c4b5f73d751dcfbb4c4b8ec5d2614c
mvltsevinc <sevincmevlut@gmail.com>
2019-03-13 23:40:50 +0300

    menus controller

--- ExpressoApi/Data/ExpressoDbContext.cs
+++ ExpressoApi/Data/ExpressoDbContext.cs
@@ -9,6 +9,11 @@ namespace ExpressoApi.Data
 {
     public class ExpressoDbContext : DbContext
     {
+        public ExpressoDbContext(DbContextOptions<ExpressoDbContext> options):base(options)
+        {
+
+        }
+
         public DbSet<Menu> Menus { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
     }
--- ExpressoApi/Startup.cs
+++ ExpressoApi/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExpressoApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,7 @@ namespace ExpressoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<ExpressoDbContext>(option => option.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExpressoDb;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
