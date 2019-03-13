bd6e2117d2879646cb184996210342e455b65f88
mvltsevinc <sevincmevlut@gmail.com>
2019-03-13 22:18:54 +0300

    adding data annotations

--- ExpressoApi/Models/Reservation.cs
+++ ExpressoApi/Models/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,26 @@ namespace ExpressoApi.Models
     public class Reservation
     {
         public int Id { get; set; }
+        
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]*$")]
         public string Phone { get; set; }
+
+        [Required]
         public int TotalPeople { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
         public string Time { get; set; }
     }
 }
