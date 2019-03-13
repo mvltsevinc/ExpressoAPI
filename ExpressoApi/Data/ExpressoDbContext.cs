9b63090dc254b995ce260666603b88e117b8d5cc
mvltsevinc <sevincmevlut@gmail.com>
2019-03-13 22:41:21 +0300

    dbcontext added

---
+++ ExpressoApi/Data/ExpressoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpressoApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressoApi.Data
+{
+    public class ExpressoDbContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
